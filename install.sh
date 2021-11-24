7e5dfad7d418b377a3093fad7a936f3e71539164
André Silva <2493377+askpt@users.noreply.github.com>
2021-11-24 18:43:05 +0000

    Added omz + config file.

--- install.sh
+++ install.sh
@@ -16,3 +16,9 @@ if test ! $(which zsh); then
     echo "Please install zsh"
     exit 1
 fi
+
+# Check for Oh My Zsh and install if we don't have it
+if test ! $(which omz); then
+    echo "Installing Oh My Zsh"
+    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
