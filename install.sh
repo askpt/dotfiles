d2b60c863ef2efd259a3b33c09f25631a3a9b195
André Silva <2493377+askpt@users.noreply.github.com>
2021-11-24 17:50:01 +0000

    Added checks for zsh and git.

--- install.sh
+++ install.sh
@@ -6,5 +6,13 @@ ARCH=$(uname -m)
 echo "Setting up your machine - $OS $ARCH"
 
 # Check if git is installed
+if test ! $(which git); then
+    echo "Please install git"
+    exit 1
+fi
 
 # Check if zsh is installed
+if test ! $(which zsh); then
+    echo "Please install zsh"
+    exit 1
+fi
