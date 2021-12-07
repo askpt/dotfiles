598f84603f9611d7d508c2a4f0bf54fe5a8f96b5
André Silva <2493377+askpt@users.noreply.github.com>
2021-12-07 22:12:34 +0000

    Removed syntax.

--- install.sh
+++ install.sh
@@ -42,11 +42,6 @@ if test ! -d $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions; then
     git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 fi
 
-if test ! -d $HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting; then
-    echo "Installing zsh-syntax-highlighting"
-    git clone git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-fi
-
 # Grab path for Homebrew
 if [ 'Linux' = "$OS" ]; then
     HOMEBREW_PATH=/home/linuxbrew/.linuxbrew/bin/brew
